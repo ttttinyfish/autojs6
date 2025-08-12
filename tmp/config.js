89204c4babd5490735b1b65c508e686f169508c6
ttttinyfish <75663581+ttttinyfish@users.noreply.github.com>
2025-08-12 12:01:38 +0800

    Update config.js
    修改默认方式为滑动解锁
    shortTimeout改为3000毫秒
    更新间隔改为100天
    
    Signed-off-by: ttttinyfish <75663581+ttttinyfish@users.noreply.github.com>

--- tmp/config.js
+++ tmp/config.js
@@ -5,7 +5,7 @@
 
 var config = {
     // 解锁配置
-    解锁方式: 1,           // 解锁方式：1=图案解锁, 2=数字密码, 其他=上滑解锁
+    解锁方式: 0,           // 解锁方式：1=图案解锁, 2=数字密码, 其他=上滑解锁
     锁屏数字密码: "000000", // 数字密码（解锁方式=2时生效）
     锁屏图案坐标: [         // 图案解锁坐标（解锁方式=1时生效）
         [284, 1479],      
@@ -27,7 +27,7 @@ var config = {
     双旗舰: 1,
     拔萝卜: 1,
     检查更新: 1,
-    更新间隔: 3, // 单位：天
+    更新间隔: 100, // 单位：天
     推送至微信 : 2, // 1 = 通过 ServerChan 推送消息到微信, 2 = 通过 PushPlus 推送消息到微信, 其它 = 不推送
     serverChanToken : "", // ServerChan Token (推送至微信 = 1 时必填)
     pushPlusToken : "",   // PushPlus Token (推送至微信 = 2 时必填)
@@ -40,10 +40,11 @@ var config = {
     capturePicPath : "./tmp/pic.png",       // 验证码截图路径
     levelRecordPath : "./tmp/level.txt",    // 成长值记录路径
     defaultTimeout : 5000,                  // 默认查找超时时间 (ms)
-    shortTimeout : 2000,                    // 较短超时时间
+    shortTimeout : 3000,                    // 较短超时时间
     retryTimes : 3,                         // 主要操作的重试次数
-    totaltime: 200                          //脚本运行时长限制(单位: 秒)
+    totaltime: 800                          //脚本运行时长限制(单位: 秒)
 
 };
 
 module.exports = config; // 导出配置
+
